0d927d8f0feba721fb0f8a431281f644ad4beabb
TimeRuner <63072819+TimeRuner@users.noreply.github.com>
2020-04-26 17:52:36 +0300

    Add files via upload

---
+++ src/js/sliders.js
@@ -0,0 +1,199 @@
+const slides = document.querySelectorAll('.sliderContainer_item');
+const dots = document.querySelector('.sliderContainer_dotsContainer');
+const dotsArea = document.querySelectorAll('.sliderContainer_element');
+const firstSlide = document.querySelector('.firstSlide_main');
+const buttons = document.querySelector('.firstSlide_container');
+const bgArr = [
+    'img/first/firstSlide.png',
+    'img/first/lastSlide.png'
+]
+const lastSlideBgImg = [
+    'img/bgPhoto/thirdSlide.png',
+    'img/fifth/lastSlideBg.png',
+    'img/fifth/lastBg.png'
+]
+const sliderContainer = document.querySelector('.sliderContainer');
+const header = document.querySelector('.header');
+const sectionFive = document.querySelector('.sectionFive');
+
+let slideIndex = 0;
+let imgBgIndex = 0;
+let wheelBottom = 0;
+let wheelTop = 0;
+let interval = 5000;
+let currentImg = 1;
+
+
+if (sliderContainer.addEventListener) {
+    if ('onwheel' in document) {
+      // IE9+, FF17+
+      sliderContainer.addEventListener("wheel", wheelingSlider);
+    } else if ('onmousewheel' in document) {
+      // устаревший вариант события
+      sliderContainer.addEventListener("mousewheel", wheelingSlider);
+    } else {
+      // Firefox < 17
+      sliderContainer.addEventListener("MozMousePixelScroll", wheelingSlider);
+    }
+  } else { // IE8-
+    sliderContainer.attachEvent("onmousewheel", wheelingSlider);
+  }
+
+
+buttons.addEventListener('click', clikedSlideBtn);
+dots.addEventListener('click', dotsSelect);          
+function showSlides(n){
+    correctSlideLength(n);
+    removeStyles();
+    addCurrentStyles(slideIndex);
+}
+function currentSlide (n){
+    showSlides(slideIndex = n);
+}
+function wheelSlide(n){
+    showSlides(slideIndex+=n);
+}
+
+function correctSlideLength(number){
+    const lastSlide = slides.length-1;
+    return slideIndex = number < 0 ? 0 : number > lastSlide ? lastSlide : number;
+}
+
+function addCurrentStyles(slideIndex){
+    switch(slideIndex){
+        case 0:
+            slides[slideIndex].classList.add('tranformTop');
+            dotsArea[slideIndex].classList.add('dotsActiveWhite');
+            dots.classList.add('dotsActiveFirst');
+            break;
+        case 4:
+            slides[slideIndex].classList.add('tranformTop');
+            dotsArea[slideIndex].classList.add('dotsActiveWhite');
+            break;
+        default:
+            slides[slideIndex].classList.add('tranformTop');
+            dotsArea[slideIndex].classList.add('dotsActiveBlack');
+            break;
+
+    }
+}
+function removeStyles(){
+    if(dots.matches('.dotsActiveFirst')){ dots.classList.remove('dotsActiveFirst');}
+    for(let i=0; i<slides.length; i++){
+        slides[i].classList.remove('tranformTop');
+    }
+    for(let i=0;i<dotsArea.length;i++){
+        if(i===0||i===4){
+            dotsArea[i].classList.remove('dotsActiveWhite');
+        }
+        else{dotsArea[i].classList.remove('dotsActiveBlack');}
+    }
+
+}
+function dotsSelect({target}){
+    for(let i = 0; i < dotsArea.length+1; i++){
+        if(target.classList.contains('sliderContainer_element')&&target==dotsArea[i]){
+            currentSlide(i);
+        }
+    }
+}
+function checkBgArrLength(index){
+    const lastElement = bgArr.length-1;
+    return imgBgIndex = index < 0 ? lastElement : index > lastElement ? 0 : index;
+}
+function changeBg(indexImg){
+    firstSlide.style.backgroundImage = 'url(' + bgArr[indexImg] + ')';
+    switch(indexImg){
+        case 0:
+            firstSlide.style.backgroundPositionY = '-60px';
+            break;
+        case 1:
+            firstSlide.style.backgroundPositionY = '-270px';  
+            break;
+        default:
+            firstSlide.style.backgroundPositionY = '0';
+            break;
+    }
+}
+function clikedSlideBtn({target}){
+    if(target.classList.contains('firstSlide_container_left')||target.classList.contains('leftImg')){
+        imgBgIndex--;
+    } else if (target.classList.contains('firstSlide_container_right')||target.classList.contains('rightImg')){
+        imgBgIndex++;
+    }
+    checkBgArrLength(imgBgIndex);
+    changeBg(imgBgIndex);
+}
+
+function wheelingSlider(e){
+    if(e.deltaY<0){
+        wheelBottom = 0;
+        wheelTop += e.deltaY;
+        header.classList.remove('displayNone');
+        firstSlide.classList.remove('smolerHeight');
+        if(wheelTop<-200){
+            if(slideIndex<5){e.preventDefault();}
+            toTop(wheelTop);
+        }
+        
+    }else {
+        wheelTop = 0;
+        wheelBottom += e.deltaY;
+        if(wheelBottom>200){
+            header.classList.add('displayNone');
+            firstSlide.classList.add('smolerHeight');
+        }
+        if(wheelBottom<3500){
+            if(slideIndex<5){e.preventDefault();}
+            toBottom(wheelBottom);
+        }
+    }
+}
+function toBottom(toBottom){
+    if(toBottom%500===0){
+        wheelSlide(1);
+    }
+}
+function toTop(toTop){
+    if(toTop%500===0){
+        wheelSlide(-1);
+    }
+}
+
+(function showAutoBg(){
+	currentImg += 1;
+	checkCurrentImg();
+    positionLastSlideBg(currentImg);
+    setTimeout(showAutoBg, interval);
+   
+}());
+
+function positionLastSlideBg(current){
+    sectionFive.style.backgroundImage = 'url(' + lastSlideBgImg[current] + ')';
+    correctWhiteBgDots(current);
+    switch(current){
+        case 1:
+            sectionFive.style.backgroundPositionY = '-270px';  
+            break;   
+        default:
+            sectionFive.style.backgroundPositionY = '0';
+            break;       
+    }
+}
+function correctWhiteBgDots(value){
+    if(value===1){
+        for(let i = 0; i<dotsArea.length; i++){
+            dotsArea[i].classList.add('dotsWhiteBg');
+        }
+    }else{
+        for(let i = 0; i<dotsArea.length; i++){
+            dotsArea[i].classList.remove('dotsWhiteBg');
+        }
+    }
+}
+function checkCurrentImg(){
+    if (currentImg > lastSlideBgImg.length-1) {
+		currentImg = 0;
+    }
+    return currentImg;
+}
